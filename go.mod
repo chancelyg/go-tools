87693b2a00748e01c4d2bb32317bb3643154b0af
Chancel.Yang <ycs1026@vip.qq.com>
2023-11-13 06:25:05 +0000

    Fix godaddy-ddns content err

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/pflag v1.0.5
 	gopkg.in/yaml.v2 v2.4.0
 )
 
--- godaddy-ddns/main.go
+++ godaddy-ddns/main.go
@@ -1,120 +1,154 @@
 package main
 
 import (
-	"bufio"
-	"flag"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
-	"os/exec"
 	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
 )
 
-func parseSSHConfig(configPath string) ([]map[string]string, error) {
-	configFile, err := os.Open(configPath)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	defer configFile.Close()
-
-	configDicts := []map[string]string{}
-	currentDict := map[string]string{}
-
-	scanner := bufio.NewScanner(configFile)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-
-		if strings.HasPrefix(line, "Host ") {
-			if len(currentDict) > 0 {
-				configDicts = append(configDicts, currentDict)
-			}
-			currentDict = map[string]string{}
-			currentDict["Host"] = strings.TrimSpace(strings.TrimPrefix(line, "Host "))
-		} else {
-			parts := strings.SplitN(line, " ", 2)
-			if len(parts) == 2 {
-				key := strings.TrimSpace(parts[0])
-				value := strings.TrimSpace(parts[1])
-				currentDict[key] = value
-			}
-		}
-	}
-
-	if len(currentDict) > 0 {
-		configDicts = append(configDicts, currentDict)
-	}
-
-	return configDicts, nil
+type DnsRecordEntity struct {
+	Data string `json:"data"`
+	TTL  int    `json:"ttl"`
 }
 
-func printSSHConfig(configs []map[string]string) {
-	fmt.Println("************************ Hi, Welcome to use Go-SSH Tool *****************************")
-	fmt.Println()
-	fmt.Println("+-----+------------------------------+-------------------------+------------------------------------------+")
-	fmt.Println("| id  | Host                         | username                | address                                  |")
-	fmt.Println("+-----+------------------------------+-------------------------+------------------------------------------+")
-	for i, config := range configs {
-		fmt.Printf("| %-3d | %-28s | %-23s | %-40s |\n", i+1, config["Host"], config["User"], config["HostName"])
+func putDNS(domain string, domainType string, domainValue string, domainNameArray []string, shopperID string, apiKey string, apiSecret string, proxyUrl string) {
+	var dnsRecord = DnsRecordEntity{
+		Data: domainValue,
+		TTL:  1800,
 	}
-	fmt.Println("+-----+------------------------------+-------------------------+------------------------------------------+")
-	fmt.Println()
-	fmt.Println("Tips: Press a number between 1 and", len(configs)-1, "to select the host to connect, or \"q\" to quit.")
-	fmt.Println()
+	var putData = [1]DnsRecordEntity{dnsRecord}
+	var putDataJson, _ = json.Marshal(putData)
+
+	// 创建一个自定义的HTTP Transport
+	var tr *http.Transport
+	if proxyUrl != "" {
+		proxy, _ := url.Parse(proxyUrl)
+		tr = &http.Transport{
+			Proxy: http.ProxyURL(proxy),
+		}
+	} else {
+		tr = &http.Transport{}
+	}
+
+	// 创建一个自定义的HTTP客户端
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   30 * time.Second,
+	}
+
+	for _, v := range domainNameArray {
+		domainUrl := fmt.Sprintf("https://api.godaddy.com/v1/domains/%s/records/%s/%s", domain, domainType, v)
+		req, err := http.NewRequest("PUT", domainUrl, bytes.NewBuffer(putDataJson))
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		req.Header.Set("Accept", "application/json")
+		req.Header.Set("X-Shopper-Id", shopperID)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("sso-key %s:%s", apiKey, apiSecret))
+
+		// 使用自定义的HTTP客户端发送请求
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			log.WithFields(log.Fields{
+				"domain":     v,
+				"domainIP":   domainValue,
+				"domainType": domainType,
+				"statusCode": resp.StatusCode,
+			}).Warning("update failed")
+			continue
+		}
+		log.WithFields(log.Fields{
+			"domain":     v,
+			"domainIP":   domainValue,
+			"domainType": domainType,
+			"statusCode": resp.StatusCode,
+		}).Info("update success")
+	}
+
+}
+
+func getIP(ipv6 bool) (string, error) {
+	url := "https://4.ipw.cn"
+	if ipv6 {
+		url = "https://6.ipw.cn"
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	//Convert the body to type string
+	var ip = string(body)
+
+	return ip, nil
+}
+
+func loggerInit() {
+	bytesWriter := &bytes.Buffer{}
+	stdoutWriter := os.Stdout
+	log.SetFormatter(&log.TextFormatter{
+		TimestampFormat: "2006-01-02T15:04:05Z",
+		FullTimestamp:   true})
+	log.SetOutput(io.MultiWriter(bytesWriter, stdoutWriter))
+	log.SetLevel(log.InfoLevel)
 }
 
 func main() {
-	configPath := flag.String("c", "$HOME/.ssh/config", "Path to SSH config file")
+	h := flag.Bool("help", false, "--help")
+	flagDomain := flag.String("domain", "", "Domain name")
+	flagType := flag.String("type", "A", "Domain name type, default is A type.")
+	flagName := flag.String("name", "", "Sub domain name, separate multiple sub domains by comma(,).")
+	flagRecord := flag.String("record", "", "Domain name corresponding value, if empty, automatically obtain IPV4/IPV6 value.")
+	flagShopperID := flag.String("shopperid", "", "Godaddy shopper id for api.")
+	flagKey := flag.String("key", "", "The key of godaddy api.")
+	flagSecret := flag.String("secret", "", "The secret of godaddy api.")
+	proxyUrl := flag.String("proxy", "", "Proxy for HTTP requests.")
+	flag.CommandLine.SortFlags = false
 	flag.Parse()
 
-	expandedPath := os.ExpandEnv(*configPath)
-	configs, err := parseSSHConfig(expandedPath)
-	if err != nil {
-		fmt.Println("Error:", err)
+	if *h {
+		flag.Usage()
 		return
 	}
 
-	printSSHConfig(configs)
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("# ")
-		scanner.Scan()
-		input := scanner.Text()
-
-		if input == "q" {
-			return
-		}
-
-		id := -1
-		fmt.Sscanf(input, "%d", &id)
-		if id >= 1 && id <= len(configs) {
-			sshConfig := configs[id-1]
-			var args []string
-			for key, value := range sshConfig {
-				if key == "Host" || key == "User" || key == "HostName" {
-					continue
-				}
-				arg := fmt.Sprintf("-o %s=%s", key, value)
-				args = append(args, arg)
-			}
-			cmdArgs := append(args, fmt.Sprintf("%s@%s", sshConfig["User"], sshConfig["HostName"]))
-			sshCmd := exec.Command("ssh", cmdArgs...)
-			sshCmd.Stdout = os.Stdout
-			sshCmd.Stdin = os.Stdin
-			sshCmd.Stderr = os.Stderr
-			err := sshCmd.Run()
-			if err != nil {
-				fmt.Println("Error:", err)
-				os.Exit(0)
-			}
-		} else {
-			fmt.Println("Error: Invalid input")
-		}
-		os.Exit(1)
-
-		printSSHConfig(configs)
+	if *flagDomain == "" || *flagName == "" || *flagShopperID == "" || *flagKey == "" || *flagSecret == "" {
+		flag.Usage()
+		log.Fatalln("please check if your parameter inputs are correct.")
 	}
+
+	loggerInit()
+
+	var nameArray []string = strings.Split(*flagName, ",")
+
+	var domainValue string = *flagRecord
+	if domainValue == "" {
+		domainValue, _ = getIP(*flagType == "AAAA")
+		if domainValue == "" {
+			log.Fatalln("get ip value error")
+		}
+	}
+	log.WithField("IP", domainValue).Info("preparing to update")
+
+	putDNS(*flagDomain, *flagType, domainValue, nameArray, *flagShopperID, *flagKey, *flagSecret, *proxyUrl)
+
 }
